472ec5cc5f7da57c620fcfbc25affb8780d2f9a9
tatjam <tatjam02@gmail.com>
2025-09-15 19:45:42 +0200

    Serious simplification of approach (WIP commit)

--- src/core.rs
+++ src/core.rs
@@ -1,7 +1,7 @@
 use std::{
     cell::OnceCell,
     collections::BTreeMap,
-    io::{Read, Seek},
+    io::{Read, Seek, SeekFrom},
     ops::Bound,
 };
 
@@ -19,6 +19,8 @@ type SampleOffset = i64;
 
 #[derive(Copy, Clone)]
 pub enum DataType {}
+
+pub struct Error {}
 
 /// Header as read from the GNU radio file
 #[derive(Copy, Clone)]
@@ -45,108 +47,79 @@ impl Header {
     }
 }
 
-/// Trait for reading when we are free to seek for headers. Allows some useful
-/// features which are impossible if the full header data is not available up front
-trait HeaderSeekerReader {
-    /// Return which header starts the segment which the sample belongs to, or None
-    /// if the sample is out of bounds.
-    fn get_header_for_sample(&self, sample: SampleIdx) -> Option<Header>;
+pub struct SampleMeta {}
+
+/// Which qualities of the current segment are guaranteed to be preserved after the seek?
+/// When in doubt, use All as most GNU Radio files are a single format and sample rate.
+enum SeekPreserve {
+    /// Allow seeking into any type of segment
+    None,
+    /// Allow seeking into segments which have the same format as the current segment
+    Format,
+    /// Allow seeking into segments which can be converted to the current segment's format
+    /// (Implies format)
+    Convertability,
+    /// Allow seeking into segments which have the same sample rate as the current segment
+    SampleRate,
+    /// Allow seeking into segments which have the same sample rate, and format which can be converted
+    /// to the one of the current segment
+    SampleRateAndConvertability,
+    /// Allow seeking into segments which have the same sample rate and format as the current one
+    All,
+    /// Only seek within the current segment. Same guarantees as All, but more restrictive.
+    Segment,
 }
 
-/// Trait for reading when we may only stream the headers. Even if headers are streamed,
-/// it's assumed they are complete!
-trait HeaderStreamReader {
-    /// Must return the header that's used for the next coming samples. The
-    /// trait implementor is responsible for its storage. Return None if no such
-    /// header can be found (EOF or nothing loaded yet)
-    fn get_current_header(&self) -> Option<Header>;
-    /// Must return how many samples remain until the current segment is finished,
-    /// or None if unknown.
-    fn get_rem_samples_in_segment(&self) -> Option<SampleCount>;
+pub trait HeaderReader {}
+
+/// Similar to Rust's Read + Seek, but obtaining individual samples instead of bytes,
+/// and with radio specific functionality (for example, you are guaranteed to never
+/// get streams with different sample rates out of this!)
+///
+/// For maximum performance, it's recommended to only ever read forwards such that all
+/// disk access is sequential. This yields maximum speed on most systems.
+pub trait SampleReadSeek<T>: HeaderReader {
+    /// Fills buf from left to right, at most filling it completely. It will read zero samples
+    /// if the next read would imply reading from:
+    /// - A segment with type not directly readable to T
+    /// - A segment with different sample rate from the one used in previous calls to read
+    ///
+    /// Returns the number of samples actually read into buf.
+    fn read(&mut self, buf: &mut [T]) -> Result<u64, Error> {}
+
+    /// Fills buf from left to right, at most filling it completely. It will read zero samples
+    /// if the next read would imply reading from:
+    /// - A segment with type inconvertible to T
+    /// - A segment with different sample rate from the one used in previous calls to read
+    ///
+    /// Returns the number of samples actually read into buf.
+    fn read_conv(&mut self, buf: &mut [T]) -> Result<u64, Error> {}
+
+    /// Returns metadata that applies to all samples read in the previous call to read
+    fn get_last_read_meta(&self) -> Option<SampleMeta> {}
+
+    /// Seeks within the file, preserving certain qualities of the current segment as
+    /// given in preserve. Returns the current position in samples from the start of the file, or
+    /// errors if the seek could not be performed, leaving the position unmodified.
+    fn seek(&mut self, pos: SeekFrom, preserve: SeekPreserve) -> Result<u64, Error> {}
+
+    /// Same as seek, but moving to segment start samples, and pos given in segments.
+    /// Returns the current position in samples from the start of the file, or errors if the
+    /// seek could not be performed, leaving the position unmodified.
+    fn seek_segment(&mut self, pos: SeekFrom, preserve: SeekPreserve) -> Result<u64, Error> {}
+
+    /// Seeks the next segment which has a format that can be converted to `T`, returning the
+    /// number of segments skipped, erroring if no such segment can be found.
+    fn seek_valid_segment(&mut self) -> Result<u64, Error> {}
 }
 
-struct StreamReaderState {
-    cur_header: Option<Header>,
-    cur_sample: SampleCount,
-    tot_sample: SampleIdx,
-}
+pub struct AttachedHeader {}
 
-/// Maps the sample index of the first sample in a header to its header
-type HeaderMap = BTreeMap<u64, Header>;
-
-/// Struct representing a GNU radio "File Meta Sink" file, without using the
-/// dettached header feature.
-/// Least performant if heavy seeking is used, due to non-sequential reading.
-struct GnuRadioAttachedHeaderFile {
-    stream_state: StreamReaderState,
-    // Lazily initialized, just in case only streaming is used (thus the interior mutability)
-    header_map: OnceCell<HeaderMap>,
-    bin_reader: Box<dyn ReadSeek>,
-}
-
-impl HeaderSeekerReader for GnuRadioAttachedHeaderFile {
-    fn get_header_for_sample(&self, sample: SampleIdx) -> Option<Header> {
-        let map = self.header_map.get_or_init(|| todo!("Implement"));
-        // In the header map, each entry maps to the first sample in its header,
-        // thus if we find the last entry to be <= to our sample, that's the
-        // one we belong to. (This is what bin trees are really efficient at!)
-        let last = map
-            .range((Bound::Unbounded, Bound::Included(sample)))
-            .next_back()?;
-
-        Some(*last.1)
-    }
-}
-
-impl HeaderStreamReader for GnuRadioAttachedHeaderFile {
-    fn get_current_header(&self) -> Option<Header> {
-        self.stream_state.cur_header
-    }
-
-    fn get_rem_samples_in_segment(&self) -> Option<SampleCount> {
-        let cur_header = self.get_current_header()?;
-        let nsamp = cur_header.get_num_samples()?;
-
-        debug_assert!(nsamp >= self.stream_state.cur_sample);
-        Some(self.stream_state.cur_sample)
-    }
-}
-
-/// Struct representing a GNU radio "File Meta Sink" file, using the
-/// dettached header feature.
-/// Functionally equivalent to GnuRadioAttachedHeaderFile, but faster.
-struct GnuRadioDettachedHeaderFile {
-    header_reader: Box<dyn ReadSeek>,
-    bin_reader: Box<dyn ReadSeek>,
-}
-
-/// Struct representing a GNU radio "File Meta Sink" file, but where
-/// the binary file is received fully sequentially without
-/// the possibility of seeking.
-/// Halfway functionality between GnuRadioAttachedHeaderFile and GnuRadioAttachedHeaderStream,
-/// but should be highly performant as the binary file is accessed only sequentially.
-struct GnuRadioDettachedHeaderStream {
-    header_reader: Box<dyn ReadSeek>,
-    bin_reader: Box<dyn Read>,
-}
-
-/// Struct representing a GNU radio "File Meta Sink" file received stream-wise.
-/// Least functionality, but guarantees fully sequential access to the source,
-/// which could be more performant.
-struct GnuRadioAttachedHeaderStream {
-    bin_reader: Box<dyn Read>,
-}
+pub struct DettachedHeader {}
 
 #[cfg(test)]
 mod core_tests {
-    
 
     #[test]
-    fn read_complex_samples_attached_file() {}
-    #[test]
-    fn read_complex_samples_dettached_file() {}
-    #[test]
-    fn read_complex_samples_attached_stream() {}
-    #[test]
-    fn read_complex_samples_dettached_stream() {}
+    fn read_complex_samples_attached() {}
 }
